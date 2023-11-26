8ad170921900988dba6882df8467c9171eef1d11
Alvaro Villalobos Vela <vivealed@gmail.com>
2023-11-26 16:30:03 -0400

    jinja

--- myapp/views.py
+++ myapp/views.py
@@ -1,27 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Project, Task
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
-# Create your views here.a
+# Create your views here.
+
+
+def home(request):
+    title = 'Django Course!!'
+    return render(request, 'index.html', {
+        'title': title
+    })
 
 
 def about(request):
-    return HttpResponse("<h3>ABOUcambio</h3>")
+    username = 'al'
+    return render(request, 'about.html', {
+        'username': username
+    })
 
 
 def projects(request):
-    projects = list(Project.objects.values())
-    return JsonResponse(projects, safe=False)
+    """ projects = list(Project.objects.values()) """
+    projects = Project.objects.all()
+    return render(request, 'projects.html', {
+        'projects': projects
+    })
 
 
 def tasks(request, id):
     task = get_object_or_404(Task, id=id)
     return HttpResponse('task = %s' % task.title)
-
-
-def home(request):
-    return HttpResponse("<h3>CASA</h3>")
 
 
 def hello(request, username):
