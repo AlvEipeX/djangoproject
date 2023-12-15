40dd3297420cecdf6d8ed062d10da40135659145
Alvaro Villalobos Vela <vivealed@gmail.com>
2023-12-15 11:08:54 -0400

    nuevo modelo de usuario

--- myapp/models.py
+++ myapp/models.py
@@ -1,7 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UsuarioPersonalizado(AbstractUser):
+    fec_nac = models.DateField(null=True, blank=True)
+    salario = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.username
 
 
 class Task(models.Model):
@@ -10,8 +18,7 @@ class Task(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     datecompleted = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(UsuarioPersonalizado, on_delete=models.CASCADE)
 
-
-"""     def __str__(self):
-        return self.title + ' - ' + self.project.name """
+    def __str__(self):
+        return self.title
